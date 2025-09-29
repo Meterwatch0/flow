ab7579ebc3589248e73480808fd4ecd582327203
Meterwatch0 <meterwatch0@gmail.com>
2025-09-29 11:21:35 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# flow
-Iot water 
+# LgCTs
---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", function (event) {
+  console.log("📦 Service Worker installed.");
+});
+
+self.addEventListener("activate", function (event) {
+  console.log("🚀 Service Worker activated.");
+});
+
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow("https://liamgabriel149.github.io/LgCTs./") // Change to your GitHub Page
+  );
+});
